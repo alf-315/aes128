764e9a40742f8c343229f9ceae3f1f41df8b58a0
Brian Curtain <dc.brian.07@gmail.com>
2020-11-16 12:17:02 +0800

    second commit

--- www/ChiperAES128.js
+++ www/ChiperAES128.js
@@ -1,5 +1,12 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (key, byte, success, error) {
+var AES256 = function () {};
+
+AES256.prototype.encrypt = function (key, byte, success, error) {
     exec(success, error, 'ChiperAES128', 'encrypt', [key, byte]);
 };
+
+var aES256 = new AES256();
+
+module.exports = aES256;
+
